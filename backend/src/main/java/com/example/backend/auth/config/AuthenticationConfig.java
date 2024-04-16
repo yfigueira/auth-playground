c2e679aa236ae4e95f8f388d33535627e8729382
yfigueira <figueirapy@gmail.com>
2024-04-16 19:15:40 +0200

    user is authenticated in db

--- backend/src/main/java/com/example/backend/auth/config/AuthenticationConfig.java
+++ backend/src/main/java/com/example/backend/auth/config/AuthenticationConfig.java
@@ -2,10 +2,14 @@ package com.example.backend.auth.config;
 
 import com.example.backend.auth.domain.UserAuthenticationRepository;
 import com.example.backend.auth.domain.UserAuthenticationService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -21,7 +25,10 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableJpaRepositories("com.example.backend.auth.persistence")
+@RequiredArgsConstructor
 public class AuthenticationConfig {
+
+    private final UserAuthenticationRepository repository;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -32,18 +39,17 @@ public class AuthenticationConfig {
                         .requestMatchers(HttpMethod.POST, "/users").permitAll()
                         .requestMatchers("/users/public").permitAll()
                         .anyRequest().authenticated())
-                .userDetailsService(userDetailsService())
+                .userDetailsService(userAuthenticationService())
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager(
-                User.withUsername("yfp")
-                        .password(passwordEncoder().encode("password"))
-                        .build()
-        );
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService) {
+        var authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetailsService);
+        authenticationProvider.setPasswordEncoder(passwordEncoder());
+        return new ProviderManager(authenticationProvider);
     }
 
     @Bean
@@ -52,7 +58,7 @@ public class AuthenticationConfig {
     }
 
     @Bean
-    public UserAuthenticationService userAuthenticationService(UserAuthenticationRepository repository) {
+    public UserAuthenticationService userAuthenticationService() {
         return new UserAuthenticationService(repository);
     }
 }
