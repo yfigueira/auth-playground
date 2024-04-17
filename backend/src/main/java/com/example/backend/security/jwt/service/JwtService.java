b85564135f165781c19b37688e9825ad055c5da6
yfigueira <figueirapy@gmail.com>
2024-04-17 15:42:32 +0200

    jwt service generates token for authenticated user

---
+++ backend/src/main/java/com/example/backend/security/jwt/service/JwtService.java
@@ -0,0 +1,33 @@
+package com.example.backend.security.jwt.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.JwtClaimsSet;
+import org.springframework.security.oauth2.jwt.JwtEncoder;
+import org.springframework.security.oauth2.jwt.JwtEncoderParameters;
+import org.springframework.stereotype.Service;
+
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+
+@Service
+@RequiredArgsConstructor
+public class JwtService {
+
+    private final JwtEncoder jwtEncoder;
+
+    public String generateToken(Authentication authentication) {
+        Instant now = Instant.now();
+
+        JwtClaimsSet claims = JwtClaimsSet.builder()
+                .issuer("self")
+                .issuedAt(now)
+                .expiresAt(now.plus(1, ChronoUnit.DAYS))
+                .subject(authentication.getName())
+                .build();
+
+        return jwtEncoder
+                .encode(JwtEncoderParameters.from(claims))
+                .getTokenValue();
+    }
+}
