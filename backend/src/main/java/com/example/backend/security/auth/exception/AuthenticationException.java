bae7ad62fb4435d8891784abf491d3142d7faa87
yfigueira <figueirapy@gmail.com>
2024-04-17 15:05:22 +0200

    minor modification

--- backend/src/main/java/com/example/backend/security/auth/exception/AuthenticationException.java
+++ backend/src/main/java/com/example/backend/security/auth/exception/AuthenticationException.java
@@ -1,12 +1,14 @@
 package com.example.backend.security.auth.exception;
 
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
 public class AuthenticationException extends RuntimeException{
 
     public AuthenticationException(String message) {
         super(message);
     }
 
-    public static AuthenticationException notFound(String username) {
-        return new AuthenticationException("%s not found".formatted(username));
+    public static UsernameNotFoundException notFound(String username) {
+        return new UsernameNotFoundException("%s not found".formatted(username));
     }
 }
