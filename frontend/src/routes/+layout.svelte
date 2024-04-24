65982e2fc4a79ec6dc8dfa8eafb094aa0e6c054b
yfigueira <figueirapy@gmail.com>
2024-04-24 16:46:07 +0200

    basic layout setup

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
     import "../app.css"
+    import michael from "$lib/assets/Michael-Jackson.svg"
 </script>
 
-<slot />
+<div class="flex flex-row h-screen w-screen">
+    <div class="h-full w-full justify-center items-center">
+        <img src={michael} alt="Michael Jackson picture" class="h-full w-full" />
+    </div>
+    <div class="bg-slate-800 h-full w-full">
+        <slot />
+    </div>
+</div>
