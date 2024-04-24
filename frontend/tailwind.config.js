db93e4251a1bfb420bfee0b7703c8b7b36bdbf8e
yfigueira <figueirapy@gmail.com>
2024-04-24 15:43:03 +0200

    frontend restart as SvelteKit project

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
+  content: [],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-};
+  plugins: [],
+}
 
